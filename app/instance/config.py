5ad8f9d5324f313a0894121572d93f1a28eeba44
Rashmi Ranjan Sahoo <54047909+rashmiranjanrrs@users.noreply.github.com>
2019-10-01 13:43:37 +0530

    Delete config.py

--- app/instance/config.py
+++
@@ -1,7 +0,0 @@
-# Initialize your database here and it shld only return an instance
-# of the database
-
-import os
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
